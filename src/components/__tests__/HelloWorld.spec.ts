f413166df4dcb501581e79623907a5a10f541c8a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-22 08:54:58 +0300

    Test push to test github actions

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -7,5 +7,4 @@ test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/program/);
 });
