0c4558b9059fce10d6357715bf30a0c10c436f81
BuildTools <unconfigured@null.spigotmc.org>
2023-08-22 09:18:03 +0300

    github actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './e2e',
+  testDir: './e2e/uitests.spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,13 +22,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-     baseURL: 'http://localhost:5173',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
 
 
   webServer: {
@@ -38,6 +32,13 @@ export default defineConfig({
     // Give it 120 seconds. Increase/decrease depending on your dev server speed.
     timeout: 120000,
     reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+     baseURL: 'http://localhost:5173',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
   },
   // ...the rest of the options...
 
