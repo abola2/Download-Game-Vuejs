e1889ca74467a09a37e9d6f01f06c21dd2661cd7
Eetu Kajanne <eetu.kajanne@gmail.com>
2023-08-23 11:41:26 +0300

    created fake_tos but it doesn't work yet

--- e2e/uitests.spec.ts
+++ e2e/uitests.spec.ts
@@ -28,6 +28,15 @@ test('test popup', async ({ page }) => {
   await expect(page.getByRole('button', { name: 'Yes', exact: true })).toHaveCount(0);
 });
 
+test('level1 complete', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'Download' }).click();
+  await page.getByRole('combobox').click();
+  await page.getByLabel('Tos').click();
+  await page.getByRole('button', { name: 'Yes', exact: true }).click();
+  await expect(page.getByRole('button', { name: 'Yes', exact: true })).toHaveCount(0);
+});
+
 
 
 
