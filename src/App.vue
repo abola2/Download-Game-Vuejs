e3c5628e50812f8660dcfd3d04ca4ec4a80814e3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-31 10:53:45 +0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -194,46 +194,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-nav {
-  width: 15%;
-  font-size: 12px;
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-30%, -50%);
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
